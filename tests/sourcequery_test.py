9b16f7fab97b3aee6cff79c9612d240271b912be
Kristoffer Dalby <kradalby@kradalby.no>
2015-12-08 06:50:37 +0100

    changed test server

--- tests/sourcequery_test.py
+++ tests/sourcequery_test.py
@@ -2,8 +2,8 @@ import unittest
 from SourceLib.SourceQuery import SourceQuery as q
 
 # This server must be up to test, kinda stupid
-TEST_SERVER = ('z.fap.no', 27015)
-TEST_SERVER_NAME = 'dfekt.no | Dust2 Only'
+TEST_SERVER = ('81.166.125.12', 27015)
+TEST_SERVER_NAME = 'Hellfragger.no #06 Office Only :: Multigamer.no'
 TEST_SERVER_GAME = 'Counter-Strike: Source'
 
 
